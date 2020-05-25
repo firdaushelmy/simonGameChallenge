3d7bf69733cd1455626cf1a9df6bddf107cb89f1
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-25 22:36:15 +0800

    added sequence function

--- game.js
+++ game.js
@@ -1,10 +1,36 @@
-var gamePattern = []
-gamePattern.push(randomChosenColour)
 
-var buttonColours = ["red", "blue", "green", "yellow"]
+// $('.btn').click(function () {
+//   $(this).fadeOut(100).fadeIn(100)
+//   playSound("." + [event.key])
+// });
+
+// function playSound(key) {
+//   switch (key) {
+//     case "greenSound":
+//       var audio = new Audio("sounds/" + randomChosenColour +".mp3")
+//       audio.play();
+//       break;
+
+//     default: console.log(innerHTML)
+//       break;
+//   }
+// }
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
 
 function nextSequence() {
-  var randomNumber = Math.round(Math.random() * 3)
+
+  var randomNumber = Math.round(Math.random() * 3);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+  audio.play();
 }
 
-var randomChosenColour = buttonColours[randomNumber]
+
+
+
