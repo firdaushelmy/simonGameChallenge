3359ee82b2b3d2fb85bd648f216c7e2c61230d98
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-25 20:50:41 +0800

    added randomise number function  and push to gamePattern

---
+++ game.js
@@ -0,0 +1,10 @@
+var gamePattern = []
+gamePattern.push(randomChosenColour)
+
+var buttonColours = ["red", "blue", "green", "yellow"]
+
+function nextSequence() {
+  var randomNumber = Math.round(Math.random() * 3)
+}
+
+var randomChosenColour = buttonColours[randomNumber]
