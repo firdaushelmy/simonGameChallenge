ce0f1d727658de6444a43a8227e2a3dae9ec5310
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-25 23:10:42 +0800

    moved code for readability

--- game.js
+++ game.js
@@ -1,4 +1,27 @@
+// user side code
 
+var userClickedPattern = []
+
+$('.btn').click(function () {
+  var userChosenColour = $(this).attr("id");
+  $("#" + userChosenColour).fadeOut(100).fadeIn(100);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern)
+});
+
+// function playSound(key) {
+//   switch (key) {
+//     case "greenSound":
+//       var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+//       audio.play();
+//       break;
+
+//     default: console.log(innerHTML)
+//       break;
+//   }
+// }
+
+// server side code
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -15,23 +38,6 @@ function nextSequence() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
   audio.play();
 }
-
-$('.btn').click(function () {
-  var userChosenColour = $(this).attr("id");
-  $("#" + userChosenColour).fadeOut(100).fadeIn(100)
-});
-
-// function playSound(key) {
-//   switch (key) {
-//     case "greenSound":
-//       var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
-//       audio.play();
-//       break;
-
-//     default: console.log(innerHTML)
-//       break;
-//   }
-// }
 
 
 
